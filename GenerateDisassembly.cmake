cfec177f79686428803c614d59c3bc10c016f647
Steven B <steve@soapforge.com>
2024-02-10 09:42:57 -0500

    Create GenerateDisassembly.cmake

---
+++ GenerateDisassembly.cmake
@@ -0,0 +1,26 @@
+function(target_generate_disassembly)
+    #do standard cmake parser
+    #set(options DO_FILTER)
+    #set(oneValueArgs "")
+    #set(multiValueArgs TARGETS)
+
+    #cmake_parse_arguments(VIEW_DISASSEMBLY "${options}" "${oneValueArgs}"
+    #"${multiValueArgs}" ${ARGN})
+
+    if(MSVC)
+        message(WARNING "Disassembly view not supported on MSVC")
+        return()
+    endif()
+
+    set(OBJDUMP_OPTS --no-addresses --no-show-raw-insn --source -l -C --visualize-jumps -d)
+        
+    foreach(TARG ${VIEW_DISASSEMBLY_TARGETS})
+        set(OUTPUT_GEXPR "$<TARGET_FILE:${TARG}>.view.s")
+        add_custom_command(TARGET ${TARG} POST_BUILD
+            COMMAND ${CMAKE_OBJDUMP} ${OBJDUMP_OPTS} $<TARGET_OBJECTS:${TARG}> > ${OUTPUT_GEXPR}
+            #BYPRODUCTS ${OUTPUT_GEXPR}
+            COMMENT "Disassembling ${TARG} to ${OUTPUT_GEXPR}"
+            VERBATIM
+        )
+    endforeach()
+endfunction()
